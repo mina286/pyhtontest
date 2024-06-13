e0ccff27527bde3336c8db32d9d0d7506ca62e07
mina samaan <minalotfy05@gamil.com>
2024-06-13 16:57:08 +0300

    creat cfile

---
+++ cflie1.py
@@ -0,0 +1,15 @@
+class Member :
+    
+    def __init__(self,first_name):
+        self.name = "mina samaan"
+
+
+memeber1 =Member("mina")
+memeber2 =Member("samaan")
+memeber3 =Member("lotfy")
+print(memeber1.name)
+print(memeber2.name)
+print(memeber3.name)
+
+
+
